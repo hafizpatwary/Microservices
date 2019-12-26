1f8000ff44b5dc5da7b5d3326d7be2d6871fc551
hafizpatwary <hafiz9711@gmail.com>
2019-12-26 14:40:35 +0000

    Added routes for outcome

--- flask_frontend/application/routes.py
+++ flask_frontend/application/routes.py
@@ -1,30 +1,39 @@
 from application import app
 from flask import request, render_template, url_for, redirect
 import requests
-import json
 
 
 @app.route('/', methods=["GET"])
 def quiz():
+    response = requests.get('http://localhost:5001')
 
-    response = requests.get('http://localhost:5001').json()
-    #prize = response["prize"]
-    image = response["image"]
-    quiz_options = response["options"]
+    if response.status_code == 200:
+        quiz = response.json()
+        image = quiz["image"]
+        quiz_options = quiz["options"]
 
-    return render_template("quiz.html", title="quiz", image=image.lower(), png=image, options=quiz_options)
+        return render_template("quiz.html", title="quiz", image=image.lower(), png=image, options=quiz_options)
+    return "Not OK"
+
+
 
 @app.route('/outcome', methods=["POST"])
 def outcome():
-    response = requests.get('http://localhost:80').json()
-    prize = response["prize"]
-    if request.form['answer'] == request.form['flag']:
-        outcome = f"Correct! You are eligible for £ {prize}0 off on your next booking with YeezyJet"
-        link = "retrieve"
-    else:
-        outcome = f"Unlucky, try again by refreshing the page!"
-        link = "quiz"
+    response = requests.get('http://localhost:80')
 
-    return render_template("outcome.html", outcome=outcome, link=link)
+    if response.status_code == 200:
+        prize = response.json()["prize"]
+        answer = request.form['answer']
+        flag = request.form['flag']
+
+        if answer == flag:
+            outcome = f"Correct! You are eligible for £ {prize}0 off on your next booking with YeezyJet"
+            link = "retrieve"
+        else:
+            outcome = "Unlucky, try again by clicking the link below!"
+            link = "quiz"
+
+        return render_template("outcome.html", outcome=outcome, link=link)
+    return redirect(url_for('quiz'))
 
 
