16cc0ffd9034f2651e6153d485d51af7e0c90a4d
Hafiz Patwary <57941389+hafizpatwary@users.noreply.github.com>
2020-01-09 09:57:29 +0000

    Update routes.py

--- flask_countries/application/routes.py
+++ flask_countries/application/routes.py
@@ -4,7 +4,7 @@ from application.functions import get_countries
 @app.route('/', methods=['GET'])
 def countries():
 
-    no_of_countries = 4
+    no_of_countries = 6
     post = get_countries('./application/countries.json', no_of_countries)
 
     return post
