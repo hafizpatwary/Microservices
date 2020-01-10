81107305a2998da6755ec53a9944455d3d861eee
hafizpatwary <hafiz9711@gmail.com>
2020-01-10 13:40:53 +0000

    uncommited some changes

--- flask_prize/tests/test_prize_service.py
+++ flask_prize/tests/test_prize_service.py
@@ -15,7 +15,9 @@ class TestBase(TestCase):
 
 
 def mocked_requests_get(*args, **kwargs):
+
     class MockResponse:
+
         def __init__(self, json_data, status_code):
             self.json_data = json_data
             self.status_code = status_code
@@ -24,64 +26,59 @@ def mocked_requests_get(*args, **kwargs):
             return self.json_data
 
     if args[0] == 'http://countries:5000/':
-    	countries_json = {
-    	"options": [
-    	{
-    	"code": "ZA",
-    	"name": "South Africa"
-    	},
-    	{
-    	"code": "TG",
-    	"name": "Togo"
-    	},
-    	{
-    	"code": "YE",
-    	"name": "Yemen"
-    	},
-    	{
-    	"code": "NL",
-    	"name": "Netherlands"
-    	}
-    	],
-    	"image": "YE"
-    	}
+        countries_json = {
+        "options": [
+        {
+        "code": "ZA",
+        "name": "South Africa"
+        },
+        {
+        "code": "TG",
+        "name": "Togo"
+        },
+        {
+        "code": "YE",
+        "name": "Yemen"
+        },
+        {
+        "code": "NL",
+        "name": "Netherlands"
+        }
+        ],
+        "image": "YE"
+        }
 
-    	return MockResponse(countries_json, 200)
+        return MockResponse(countries_json, 200)
 
     elif args[0] == 'http://temperature:5000/':
-    	return MockResponse({"temperature": "5.2"}, 200)
+        return MockResponse({"temperature": "5.2"}, 200)
 
     return MockResponse(None, 404)
 
 
-class TestQuizFunction(TestBase):
+class TestServiceFunction(TestBase):
 
-	# To patch 'requests.get' with our own method. The mock object is passed in to our test case method.
-	@mock.patch('requests.get', side_effect=mocked_requests_get)
-	def test_quiz_route(self, mock_get):
-        """ Test login required to create diet """
+    # To patch 'requests.get' with our own method. The mock object is passed in to our test case method.
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_prize(self, mock_get):
+        country_service = 'http://countries:5000/'
+        temperature_service = 'http://temperature:5000/'
 
-        target_url = url_for('quiz')
-        response = self.client.get(target_url)
+        prize_json = routes.get_prize(country_service, temperature_service)
+        prize = json.loads(prize_json)
+        prize_money = float(prize["prize"])
 
-        self.assertEqual(response.status_code, 200)
+        self.assertTrue(prize_money >= 0)
 
-	def test_get_countries_exception(self):
-		
-		with pytest.raises(Exception):
-			assert routes.get_prize('http://countries:5000/', 'http://temperature:5000/')
+    def test_get_countries_exception(self):
+        
+        with pytest.raises(Exception):
+            assert routes.get_prize('http://countries:5000/', 'http://temperature:5000/')
 
 
 class TestServiceRoutes(TestBase):
 
-	@mock.patch('requests.get', side_effect=mocked_requests_get)
-	def test_api(self, mock_get):
-		response = self.client.get(url_for('prize'))
-		self.assertEqual(response.status_code, 200)
-
-
-
-
-
-
-
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_api(self, mock_get):
+        response = self.client.get(url_for('prize'))
+        self.assertEqual(response.status_code, 200)
