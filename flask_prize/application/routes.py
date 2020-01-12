ad5b57a561ba0466095e2e536ce68e5b453ebe25
hafizpatwary <hafiz9711@gmail.com>
2020-01-12 12:24:03 +0000

    added temperature to Json package

--- flask_prize/application/routes.py
+++ flask_prize/application/routes.py
@@ -20,7 +20,7 @@ def get_prize(country_service, temperature_service):
             data = {
                 "prize":prize,
                 "city":f"{temperature['city']}",
-                "temperature":f"{temperature['temperature']}"
+                "temperature":f"{temp}"
             }
 
             return json.dumps(data, indent=2)
