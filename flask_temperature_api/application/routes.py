4575fdae1313413127b3198f4189272f4ba81afb
hafizpatwary <hafiz9711@gmail.com>
2020-01-12 11:32:30 +0000

    city added to json package

--- flask_temperature_api/application/routes.py
+++ flask_temperature_api/application/routes.py
@@ -10,7 +10,7 @@ def get_temerature(cities, api_key):
     weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={api_key}").json()
     temperature = float(weather['main']['temp']) - 273.15 # Transforming temperature in degree *C
 
-    package = {"temperature":f"{temperature}"}
+    package = {"temperature":f"{temperature}", "city":f"{random_city}"}
 
     return json.dumps(package, indent=2)
 
